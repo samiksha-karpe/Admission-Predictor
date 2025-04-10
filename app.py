eb50d4574258d53e84a84e36b061646b59893811
samiksha-karpe <karpeasamiksha@gmail.com>
2025-04-11 00:26:12 +0530

    final commit

--- app.py
+++ app.py
@@ -35,6 +35,6 @@ def predict():
     return render_template("index.html", result=result)
 
 # Use 0.0.0.0 and a dynamic port for Render
-if __name__ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
